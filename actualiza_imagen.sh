fbda09629dd25f0810bc9130ffe6559e49738cca
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-28 12:20:32 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -10,7 +10,7 @@
                         cp -R /home/pi/EXPERIMENTAL/* /home/pi/A108                                             
                         sudo chmod 777 -R /home/pi/A108
                                      
-                        #cp -R /home/pi/EXPERIMENTAL/qt/ /home/pi/A108/                                               
-                        #sudo chmod 777 -R /home/pi/A108/qt
+                        cd /home/pi/A108/qt/
+                        ./qt_popus_actualizada &
 
                         
