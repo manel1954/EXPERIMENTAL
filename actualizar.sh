10e0bb86ce546338a4d917c2b266f3f581f6efe8
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-31 15:46:51 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -15,14 +15,7 @@ fi
 
 dvswitch=$(awk "NR==18" /home/pi/status.ini)
 if [ "$dvswitch" = 'DVSWITCH=OFF' ];then
-sudo systemctl stop ysfgateway.service
-sudo systemctl stop dmr2ysf.service
-sudo systemctl stop analog_bridge.service
-sudo systemctl stop ircddbgateway.service
-sudo systemctl stop md380-emu.service
-sudo systemctl stop mmdvm_bridge.service
-sudo systemctl stop nxdngateway.service
-sudo systemctl stop p25gateway.service
+sudo systemctl stop ircddbgatewayd.service
 else
 echo "no hace nada"  
 fi
