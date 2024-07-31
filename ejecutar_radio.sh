b5d438a6371a2cb32e74fc0224b20de1b34968d6
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-31 23:56:58 +0200

    Update ejecutar_radio.sh

--- ejecutar_radio.sh
+++ ejecutar_radio.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/pi/MMDVMHost
-xterm -geometry 77x12+14+64 -bg brown -fg white -fa 'Serif' -fs 9x -T BRANDMEISTER -e sudo ./MMDVMHost MMDVM.ini &
+xterm -geometry 77x12+14+64 -bg brown -fg white -fa 'Serif' -fs 9x -T DMO -e sudo ./MMDVMHost MMDVM.ini &
 
