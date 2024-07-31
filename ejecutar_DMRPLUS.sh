3b0a3c5d209316b9c00a719314c5a0934e442b68
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-01 00:10:19 +0200

    Update ejecutar_DMRPLUS.sh

--- ejecutar_DMRPLUS.sh
+++ ejecutar_DMRPLUS.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xterm -geometry 77x12+14+64 -bg black -fg yellow -fa 'Serif' -fs 9x -T DMR+ -e sudo ./MMDVMPLUS MMDVMPLUS.ini &
+xterm -geometry 77x12+14+64 -bg black -fg yellow -fa 'Serif' -fs 9x -T DMR_PLUS -e sudo ./MMDVMPLUS MMDVMPLUS.ini &
 
