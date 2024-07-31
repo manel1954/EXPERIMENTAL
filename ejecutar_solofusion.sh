f1fc7e07f6b1636c43d3b304a1faaa4e4747acf0
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-31 21:09:07 +0200

    Update ejecutar_solofusion.sh

--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-cd /home/pi/YSFClients/YSFGateway
-xterm -geometry 77x6+14+70 -bg brown -fg white -fa 'Serif' -fs 9x -T GATEWAY -e sudo ./YSFGateway YSFGateway.ini &
-cd /home/pi/MMDVMHost
-sudo ./MMDVMFUSION MMDVMFUSION.ini
+# cd /home/pi/YSFClients/YSFGateway
+# xterm -geometry 77x6+14+70 -bg brown -fg white -fa 'Serif' -fs 9x -T GATEWAY -e sudo ./YSFGateway YSFGateway.ini &
+# cd /home/pi/MMDVMHost
+# sudo ./MMDVMFUSION MMDVMFUSION.ini
 
+cd /home/pi/YSFClients/YSFGateway
+xterm -geometry 77x6+14+70 -bg black -fg orange -fa ‘verdana’ -fs 9x -T CONSOLA_YSFGateway -e ./YSFGateway YSFGateway.ini & 
+cd /home/pi/MMDVMHost
+xterm -geometry 77x6+14+64 -bg black -fg orange -fa ‘verdana’ -fs 9x -T CONSOLA_SOLOFUSION -e sudo ./MMDVMFUSION MMDVMFUSION.ini
 
