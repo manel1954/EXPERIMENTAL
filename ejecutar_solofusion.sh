7364e3e0fceac102a752953c37079905fa194244
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-31 20:33:02 +0200

    Update ejecutar_solofusion.sh

--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -1,58 +1,9 @@
 #!/bin/bash
 
-mode=`grep -n -m 1 "^UARTPort=" /home/pi/MMDVMHost/MMDVMFUSION.ini`
-buscar=":"
-caracteres=`expr index $mode $buscar`
-caracteres_linea=`expr $caracteres - 1`
-numero_linea_port=`expr substr $mode 1 $caracteres_linea`
-mode=$(awk "NR==$numero_linea_port" /home/pi/MMDVMHost/MMDVMFUSION.ini)
-puerto=`expr substr $mode 15 14`
-puerto="  "$puerto
-cd /home/pi/Desktop
-sudo cp RXF_SOLOFUSION.desktop /home/pi
-frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMFUSION.ini)
-frecuencia=`expr substr $frecuencia 13 9`
-frecuencia=$frecuencia$puerto
-sed -i "11c Name=$frecuencia" /home/pi/RXF_SOLOFUSION.desktop
-
-sudo cp /home/pi/RXF_SOLOFUSION.desktop /home/pi/Desktop
-
-sudo rm /home/pi/RXF_SOLOFUSION.desktop
-
-#Escribe en el fichero INFO_RXF para poner los datos en el icono INFO TXF                        
-sed -i "6c $frecuencia" /home/pi/INFO_RXF
-
-SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-cd /home/pi/Desktop
-sudo cp Abrir_solofusion.desktop /home/pi
-sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh cerrar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
-sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_YSF_ON.png" /home/pi/Abrir_solofusion.desktop
-sed -i "11c Name[es_ES]=Cerrar solo FUSION" /home/pi/Abrir_solofusion.desktop
-sed -i "12c SOLOFUSION=ON" /home/pi/status.ini
-cd /home/pi
-sudo cp Abrir_solofusion.desktop /home/pi/Desktop
-
-sudo rm /home/pi/Abrir_solofusion.desktop
-
-echo "\33[38;5;138m"
-echo " **************************************************************************"
-echo "                          ABRIENDO SOLO FUSION                             "
-echo " **************************************************************************"
-sleep 2
 cd /home/pi/YSFClients/YSFGateway
-sudo lxterminal --geometry=88x12 -e ./YSFGateway YSFGateway.ini & 
+sudo xterm -geometry 77x6+14+72 -bg brown -fg white -fa 'Serif' -fs 9x -T GATEWAY -e ./YSFGateway YSFGateway.ini & 
 cd /home/pi/MMDVMHost
-#/home/pi/A108/./qt_info_solofusion & sudo ./MMDVMFUSION MMDVMFUSION.ini
 sudo ./MMDVMFUSION MMDVMFUSION.ini
-cd /home/pi/Desktop
-sudo cp Abrir_solofusion.desktop /home/pi
+    system("cd /home/pi/A108; xterm -geometry 77x6+14+64 -bg brown -fg white -fa 'Serif' -fs 9x -T SOLOFUSION -e sh ejecutar_solofusion.sh &");
 
-sed -i "6c Exec=sh -c 'cd /home/pi/A108/;lxterminal --geometry=88x19 -e sudo sh ejecutar_solofusion.sh'" /home/pi/Abrir_solofusion.desktop
-sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_YSF.png" /home/pi/Abrir_solofusion.desktop
-sed -i "11c Name[es_ES]=Abrir solo FUSION" /home/pi/Abrir_solofusion.desktop
-sed -i "12c SOLOFUSION=OFF" /home/pi/status.ini
-cd /home/pi
-sudo cp Abrir_solofusion.desktop /home/pi/Desktop
-
-sudo rm /home/pi/Abrir_solofusion.desktop
 
