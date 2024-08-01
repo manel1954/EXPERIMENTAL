c1aa0a26b130d3503c52d32d67d4e590ee17e521
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-01 12:49:05 +0200

    Update ejecutar_bm.sh

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMBM.ini)
+frecuencia=`expr substr $frecuencia 13 9`
+sed -i "70c $frecuencia" /home/pi/status.ini
+
+puerto=$(awk "NR==51" /home/pi/MMDVMHost/MMDVMBM.ini)
+puerto=`expr substr $puerto 15 14`
+sed -i "71c $puerto" /home/pi/status.ini
+
 cd /home/pi/MMDVMHost
 xterm -geometry 77x12+14+64 -bg brown -fg white -fa 'Serif' -fs 9x -T BRANDMEISTER -e sudo ./MMDVMBM MMDVMBM.ini &
