0ea7a7beabc7a12395028046698c7578e9d2847a
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-01 12:23:44 +0200

    Update ejecutar_radio.sh

--- ejecutar_radio.sh
+++ ejecutar_radio.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+mode=$(awk "NR==77" /home/pi/MMDVMHost/MMDVM.ini)
+puerto=`expr substr $mode 15 14`
+
+frecuencia=$(awk "NR==76" /home/pi/MMDVMHost/MMDVM.ini)
+frecuencia=`expr substr $frecuencia 13 9`
+
 cd /home/pi/MMDVMHost
 xterm -geometry 77x12+14+64 -bg blue -fg white -fa 'Serif' -fs 9x -T DMO -e sudo ./MMDVMHost MMDVM.ini &
 
