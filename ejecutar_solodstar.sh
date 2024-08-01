5df7224d372d6a8c3f02dd21357f86348d89ebfa
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-01 12:58:44 +0200

    Update ejecutar_solodstar.sh

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMDSTAR.ini)
+frecuencia=`expr substr $frecuencia 13 9`
+sed -i "80c $frecuencia" /home/pi/status.ini
+
+puerto=$(awk "NR==51" /home/pi/MMDVMHost/MMDVMDSTAR.ini)
+puerto=`expr substr $puerto 15 14`
+sed -i "81c $puerto" /home/pi/status.ini
+
+
 sudo systemctl stop ircddbgatewayd.service
 
 # Ejecuta Solo D-STAR 
