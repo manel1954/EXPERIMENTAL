c4d4718d114a8c6fca3dd39b38c38c935b9fa853
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-07 22:03:00 +0200

    Update ejecutar_DMRPLUS.sh

--- ejecutar_DMRPLUS.sh
+++ ejecutar_DMRPLUS.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+status_dmrplus="NR==6" /home/orangepi/.local/autoarranque.ini)
 
+if [ "$status_dmrplus" = "DMR+=ON" ];then
+sed -i "4c Exec=sh -c 'cd /home/pi/A108; sh cerrar_DMRPLUS.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "5c Icon=/home/pi/A108/ICONO_DMRPLUS_ON.png" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "10c Name[es_ES]=Cerrar DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
+//sed -i "16c MMDVMPLUS=ON" /home/pi/status.ini
+else
+echo ""
+fi
 frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMPLUS.ini)
 frecuencia=`expr substr $frecuencia 13 9`
 sed -i "72c $frecuencia" /home/pi/status.ini
