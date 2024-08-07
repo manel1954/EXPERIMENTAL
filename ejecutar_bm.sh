067c21bc9d9d9458afe2bed5b9187c6a46d0e216
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-07 22:43:06 +0200

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sed -i "4c Exec=sh -c 'cd /home/pi/A108; sh cerrar_DMRPLUS.sh" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "5c Icon=/home/pi/A108/ICONO_DMRPLUS_ON.png" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "10c Name[es_ES]=Cerrar DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "6c MMDVMPLUS=ON" /home/pi/status.ini
+
 frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMBM.ini)
 frecuencia=`expr substr $frecuencia 13 9`
 sed -i "70c $frecuencia" /home/pi/status.ini
