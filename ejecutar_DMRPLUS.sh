19b71cfe18fbcc2d0822776b2d82fd1d2379ecad
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-08 12:43:26 +0200

    Update ejecutar_DMRPLUS.sh

--- ejecutar_DMRPLUS.sh
+++ ejecutar_DMRPLUS.sh
@@ -13,7 +13,7 @@ puerto=$(awk "NR==51" /home/pi/MMDVMHost/MMDVMPLUS.ini)
 puerto=`expr substr $puerto 15 14`
 sed -i "73c $puerto" /home/pi/status.ini
 
-x=$(awk "NR==90" /home/pi/status.ini)
+x=$(awk "NR==91" /home/pi/status.ini)
 
 cd /home/pi/MMDVMHost
 xterm -geometry 77x12+$x+64 -bg black -fg white -fa 'Serif' -fs 9x -T DMR_PLUS -e sudo ./MMDVMPLUS MMDVMPLUS.ini &
