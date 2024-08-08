8c8b487fe5ab954b1799d6f1a596c7ba4a0ac9d3
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-08 22:30:30 +0200

    Update ejecutar_bm.sh

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sed -i "4c Exec=sh -c 'cd /home/pi/A108; sh cerrar_DMRPLUS.sh" /home/pi/Abrir_MMDVMPLUS.desktop
-sed -i "5c Icon=/home/pi/A108/ICONO_DMRPLUS_ON.png" /home/pi/Abrir_MMDVMPLUS.desktop
-sed -i "10c Name[es_ES]=Cerrar DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
-sed -i "6c MMDVMPLUS=ON" /home/pi/status.ini
+sed -i "4c Exec=sh -c 'cd /home/pi/A108; sh cerrar_bm.sh" /home/pi/Abrir_MMDVMBM.desktop
+sed -i "5c Icon=/home/pi/A108/ICONO_BM_ON.png" /home/pi/Abrir_MMDVMBM.desktop
+sed -i "10c Name[es_ES]=Cerrar BM" /home/pi/Abrir_MMDVMBM.desktop
+sed -i "7c MMDVMBM=ON" /home/pi/status.ini
 
 frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMBM.ini)
 frecuencia=`expr substr $frecuencia 13 9`
@@ -13,7 +13,9 @@ puerto=$(awk "NR==51" /home/pi/MMDVMHost/MMDVMBM.ini)
 puerto=`expr substr $puerto 15 14`
 sed -i "71c $puerto" /home/pi/status.ini
 
+x=$(awk "NR==90" /home/pi/status.ini)
+
 cd /home/pi/MMDVMHost
-xterm -geometry 77x12+14+64 -bg brown -fg white -fa 'Serif' -fs 9x -T BRANDMEISTER -e sudo ./MMDVMBM MMDVMBM.ini &
+xterm -geometry 77x12+$x+64 -bg brown -fg white -fa 'Serif' -fs 9x -T BRANDMEISTER -e sudo ./MMDVMBM MMDVMBM.ini &
 
-
+ 
