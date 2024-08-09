4266f2717c9c075a13816e0d9e5cb8f51eb8b11f
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-09 20:30:45 +0200

    Update borrar_logs.sh

--- borrar_logs.sh
+++ borrar_logs.sh
@@ -20,6 +20,6 @@ sudo rm /var/log/mmdvm/MMDVM*
 # sudo rm /home/pi/DMRGateway/*.log
 # clear
 #cd /home/pi/A108
-xterm -geometry 77x12+643+64 -bg black -fg white -fa 'Serif' -fs 9x -T BORRANDO_LOGS -e sh borrar_logs.sh &
+xterm -geometry 77x12+643+64 -bg black -fg white -fa 'Serif' -fs 9x -T BORRANDO_LOGS -e sh borrar_logs.sh
 
 sleep 5
