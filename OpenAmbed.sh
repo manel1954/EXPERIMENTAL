2af6d65e0fe598c92a1834eccc4cfd45273d2b12
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-10 23:18:08 +0200

    Update OpenAmbed.sh

--- OpenAmbed.sh
+++ OpenAmbed.sh
@@ -8,4 +8,4 @@ sudo rmmod ftdi_sio
 sudo rmmod usbserial
 
 
-sudo xterm -geometry 61x16+249+97 -bg black -fg white -fa 'Serif' -fs 9x -T STICK33_AMBED -e sudo ambed 192.168.1.44 &
+sudo xterm -geometry 61x12+249+97 -bg black -fg white -fa 'Serif' -fs 9x -T STICK33_AMBED -e sudo ambed 192.168.1.44 &
