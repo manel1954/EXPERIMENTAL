faa251441230a29740cbaac20b7379e5e4480696
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-12 09:48:38 +0200

    Update OpenAmbed.sh

--- OpenAmbed.sh
+++ OpenAmbed.sh
@@ -7,5 +7,5 @@ sed -i '26c AMBE=ON' /home/pi/status.ini
 
 # sudo rmmod usbserial
 
-
+cd /home/pi/AMBEServer
 sudo xterm -geometry 77x12+1287+62 -bg black -fg white -fa 'Serif' -fs 9x -T AMBE_SERVER -e sudo ./AMBEserver -p 24600 &
