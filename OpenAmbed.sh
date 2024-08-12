1eb24f75d3f45923d4a7b68fff3206254d354176
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-12 12:43:32 +0200

    Update OpenAmbed.sh

--- OpenAmbed.sh
+++ OpenAmbed.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sed -i '26c AMBE=ON' /home/pi/status.ini
+
 puerto_router=$(awk "NR==1" /home/pi/.local/ambe_server.ini) 
 puerto_modem=$(awk "NR==2" /home/pi/.local/ambe_server.ini)
 baut_rate=$(awk "NR==3" /home/pi/.local/ambe_server.ini)
 cd /home/pi/AMBEServer
 sudo xterm -geometry 77x12+1287+62 -bg black -fg white -fa 'Serif' -fs 9x -T AMBE_SERVER -e sudo ./AMBEserver -p $puerto_router -i $puerto_modem -s $baut_rate &
+sed -i '26c AMBE=ON' /home/pi/status.ini
