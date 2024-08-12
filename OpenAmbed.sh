2913036630290c9358c425f764ab1d926cfb6165
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-12 10:16:38 +0200

    Update OpenAmbed.sh

--- OpenAmbed.sh
+++ OpenAmbed.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-#cd /home/pi/release/build/xlx_ambed
-#!/bin/bash
-# start ambed server
+
 sed -i '26c AMBE=ON' /home/pi/status.ini
-# sudo rmmod ftdi_sio
-
-# sudo rmmod usbserial
-
+puerto_router=$(awk "NR==1" /home/pi/.local/ambe_server.ini) 
+puerto_modem=$(awk "NR==2" /home/pi/.local/ambe_server.ini)
+baut_rate=$(awk "NR==3" /home/pi/.local/ambe_server.ini)
 cd /home/pi/AMBEServer
-sudo xterm -geometry 77x12+1287+62 -bg black -fg white -fa 'Serif' -fs 9x -T AMBE_SERVER -e sudo ./AMBEserver -p 24600 &
+sudo xterm -geometry 77x12+1287+62 -bg black -fg white -fa 'Serif' -fs 9x -T AMBE_SERVER -e sudo ./AMBEserver -p $puerto_router -i $puerto_modem -s $baut_rate &
