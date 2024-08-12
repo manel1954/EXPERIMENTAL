3cf69f832fcc5dabb0a1328462a9aff699ee1178
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-12 18:26:43 +0200

    Update actualizar_ids.sh

--- actualizar_ids.sh
+++ actualizar_ids.sh
@@ -1,21 +1,5 @@
 ﻿#!/bin/bash
-#Colores
-ROJO="\033[1;31m"
-VERDE="\033[1;32m"
-BLANCO="\033[1;37m"
-AMARILLO="\033[1;33m"
-CIAN="\033[1;36m"
-GRIS="\033[0m"
-MARRON="\33[38;5;138m"
-NEGRO="\33[0;30m"
-clear
-echo ""
-echo -n "${AMARILLO}"
-echo "    *******ACTUALIZANDO IDS INDICATIVOS**25-01-2023"
-echo -n "${VERDE}"
-#echo "ACTUALIZANDO DMRIds.dat para DVSWITCH"
-sleep 13
-echo "${NEGRO}"
+
 var=`grep -n -m 1 '\<DMRIDPATH\>' /home/pi/MMDVMHost/linux/DMRIDUpdate.sh`
 buscar=":"
 largo_linea=`expr index $var $buscar`
