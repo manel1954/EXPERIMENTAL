b5352ae4cc1c40b123968acee3e5e78e0084e90e
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-14 11:53:15 +0200

    Update ejecutar_bm.sh

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -16,6 +16,6 @@ sed -i "71c $puerto" /home/pi/status.ini
 x=$(awk "NR==90" /home/pi/status.ini)
 
 cd /home/pi/MMDVMHost
-xterm -geometry 76x10+$x+64 -bg brown -fg white -fa 'arial' -fs 10x -T BRANDMEISTER -e sudo ./MMDVMBM MMDVMBM.ini &
+xterm -geometry 76x9+$x+64 -bg brown -fg white -fa 'arial' -fs 11x -T BRANDMEISTER -e sudo ./MMDVMBM MMDVMBM.ini &
 
  
