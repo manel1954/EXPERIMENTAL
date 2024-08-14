d32fb30743e67e5e1b5476b8f26ebea8074ac6cf
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-14 11:52:16 +0200

    1

--- ejecutar_ESPECIAL.sh
+++ ejecutar_ESPECIAL.sh
@@ -9,4 +9,4 @@ puerto=`expr substr $puerto 15 14`
 sed -i "75c $puerto" /home/pi/status.ini
 
 cd /home/pi/MMDVMHost
-xterm -geometry 77x12+14+64 -bg black -fg yellow -fa 'Serif' -fs 9x -T ESPECIAL -e sudo ./MMDVMESPECIAL MMDVMESPECIAL.ini &
+xterm -geometry 77x12+14+64 -bg black -fg yellow -fa 'arial' -fs 9x -T ESPECIAL -e sudo ./MMDVMESPECIAL MMDVMESPECIAL.ini &
