b5425d2815b696a1cb3f8afdec2e27876ef0637c
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-15 11:10:43 +0200

    Update ejecutar_solodstar.sh

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -8,10 +8,19 @@ puerto=$(awk "NR==51" /home/pi/MMDVMHost/MMDVMDSTAR.ini)
 puerto=`expr substr $puerto 15 14`
 sed -i "81c $puerto" /home/pi/status.ini
 
-
-sudo systemctl stop ircddbgatewayd.service
+x=$(awk "NR==94" /home/pi/status.ini)
 
 # Ejecuta Solo D-STAR 
+sudo systemctl stop ircddbgatewayd.service
+sudo ircddbgateway -gui &
+
 cd /home/pi/MMDVMHost
-sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini
+xterm -geometry 76x10+$x+764  -bg black -fg cyan -fa 'arial' -fs 10x -T SOLO_DSTAR -e sudo ./MMDVMDSTAR MMDVMDSTAR.ini &
+
+# sudo systemctl stop ircddbgatewayd.service
+
+# # Ejecuta Solo D-STAR 
+# sudo ircddbgateway -gui &
+# cd /home/pi/MMDVMHost
+# sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini
 
