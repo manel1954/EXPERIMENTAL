d14278b27e6fff935009ae39dc2163f29e01d8ad
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-15 11:04:53 +0200

    1

--- ejecutar_radio.sh
+++ ejecutar_radio.sh
@@ -8,7 +8,7 @@ puerto=$(awk "NR==51" /home/pi/MMDVMHost/MMDVM.ini)
 puerto=`expr substr $puerto 15 14`
 sed -i "77c $puerto" /home/pi/status.ini
 
-x=$(awk "NR==90" /home/pi/status.ini)
+x=$(awk "NR==93" /home/pi/status.ini)
 
 cd /home/pi/MMDVMHost
-xterm -geometry 76x10+$x+64  -bg black -fg cyan -fa 'Serif' -fs 10x -T DMO -e sudo ./MMDVMHost MMDVM.ini &
+xterm -geometry 76x10+$x+64  -bg black -fg cyan -fa 'arial' -fs 10x -T DMO -e sudo ./MMDVMHost MMDVM.ini &
