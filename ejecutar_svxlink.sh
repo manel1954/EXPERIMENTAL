be2b226b56798deb81b496860ead21ee80e2648b
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-08-19 19:14:53 +0200

    Update ejecutar_svxlink.sh

--- ejecutar_svxlink.sh
+++ ejecutar_svxlink.sh
@@ -16,7 +16,7 @@ sed -i "8c SVXLINK=ON" /home/pi/status.ini
 
 
 
-xterm -geometry 78x17+1283+745 -bg black -fg yellow -fa 'serift' -fs 9x -T CONSOLA_SVXLINK -e sudo svxlink
+xterm -geometry 88x17+1285+745 -bg black -fg yellow -fa 'serift' -fs 9x -T CONSOLA_SVXLINK -e sudo svxlink
 
 
 # cd /home/pi/Desktop
