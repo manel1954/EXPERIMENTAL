0c794798a51e6638e97b89c23db4a639aecb43db
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-09-25 08:58:12 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
                         cd /home/pi/EXPERIMENTAL                      
-                        git pull --force  
-                        #cd /home/pi/A108/qt/
-                        #./qt_popus_actualizada &                     
+                        #git pull --force 
+
+                        git config --global pull.rebase false 
+                    
                         sudo rm -R /home/pi/A108
                         mkdir /home/pi/A108                                                
                         cp -R /home/pi/EXPERIMENTAL/* /home/pi/A108                                             
