c621cb00f25ed0d3935aa25c0833e816811feda9
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-09-25 09:03:38 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
                         cd /home/pi/EXPERIMENTAL                      
-                        git pull --force  
-
-                        #git config --global pull.rebase false 
-                    
+                        git pull --force                      
                         sudo rm -R /home/pi/A108
                         mkdir /home/pi/A108                                                
                         cp -R /home/pi/EXPERIMENTAL/* /home/pi/A108                                             
